44ad99440ecb787eff63c633eca5d60077a6daf4
Antony Ghayes <84248602+AntonyAGA@users.noreply.github.com>
2021-05-15 22:34:53 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,1 +1,13 @@
+#Check for updstes
+cd $HOME
 apt update && apt upgrade
+#install packages
+pkg install git wget python python2 python3 nano unzip zip php curl hydra ruby perl proot nmap zsh man -y
+#install some packages with pip
+pip install lolcat
+pip2 install requestes
+pip2 install mechanize
+pip2 install bundler
+pip2 install colorama
+
+echo "Ask for Storage permission"
